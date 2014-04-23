e667139c6eae22dc5772cfbcc88ce7a2542aadac
urthbound <jeffowler@gmail.com>
2014-04-22 22:49:59 -0400

    mid post

--- src/genotic/core.clj
+++ src/genotic/core.clj
@@ -1,28 +1,47 @@
 (ns genotic.core)
 
 (defn new-gene
-  ([] (new-gene 1000))
+  ([] (new-gene 50))
   ([length]
-   (take length (repeatedly #(rand-int 2)))))
+   ; (take length (repeatedly #(rand-int 2)))))
+   (take length (repeat 0))))
 
 (defn generation []
    (take 100 (repeatedly #(new-gene))))
 
+
 (defn fitness [gene]
   (count (filter #(= 1 %) gene)))
 
-(defn maxer [generation]
-  (apply max (map #(fitness %) generation)))
-
 (defn evaluate-generation [generation]
-  (sort-by #(fitness %) generation))
+  (let [n (/ (count generation) 2)]
+    (take n
+      (reverse
+        (sort-by
+          #(fitness %)
+          generation)))))
 
-(evaluate-generation tester)
+(defn mutate [g]
+  (if (= (rand-int 100) 50)
+    (bit-flip g 0)))
+
+(defn mutate-gen [generation]
+  (map #(map mutate %) generation))
+
+(defn tng [generation]
+  (let [ng (evaluate-generation generation)]
+    (concat ng
+            (mutate-gen ng))))
 
 
+(defn go [generation]
+  (if (= (fitness (first (evaluate-generation generation))) 50)
+    (println (first (evaluate-generation generation)))
+    (do
+      (println (first (evaluate-generation generation)))
+      ; (println (fitness (first (evaluate-generation generation))))
+      (recur (tng generation)))))
 
-(def tester (generation))
-(maxer tester)
 
-
-(generation)
+(defn -main []
+  (go (generation)))
